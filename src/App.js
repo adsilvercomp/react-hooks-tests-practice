eb36d915175a78106c63c74100e9ee9deb5eec1d
Aaron Silverstein <adsilvercomp@Aarons-MacBook-Pro.local>
2021-06-14 23:54:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [buttonColor, setColor] = useState('red')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-testid="background" className={`App ${buttonColor}`}>
+      <button onClick={()=> buttonColor === 'red'? setColor('blue'): setColor('red')}  name="change-color-button" className="change-color">Change Color</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('check if button is on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorChangeButton = screen.getByRole('button');
+  expect(colorChangeButton).toBeInTheDocument();
 });
+
+test('test if background color changes on click', () => {
+  render(<App/>);
+  const background = screen.getByTestId('background');
+
+  expect(background.classList.contains('red')).toBe(true)
+  
+  fireEvent(
+    screen.getByRole('button'),
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    })
+  )
+
+  expect(background.classList.contains('blue')).toBe(true)
+
+  fireEvent(
+    screen.getByRole('button'),
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    })
+  )
+
+  expect(background.classList.contains('red')).toBe(true) 
+
+})
